dd2a4e5e27ac3740f60725e79a8575604f89f735
Nazwa Hafidzah <nazwahafidzah301@gmail.com>
2025-05-02 20:35:46 +0700

    Fix: remove form comment block

--- resources/views/mahasiswa/edit.blade.php
+++ resources/views/mahasiswa/edit.blade.php
@@ -1,10 +1,12 @@
-<!-- <form method="POST" action="{{ route('mahasiswa.update', $mhs->id) }}">
-    @csrf @method('PUT')
+<form method="POST" action="{{ route('mahasiswa.update', $mhs->id) }}">
+    @csrf
+    @method('PUT')
     Nama: <input type="text" name="nama" value="{{ $mhs->nama }}"><br>
     NIM: <input type="text" name="nim" value="{{ $mhs->nim }}"><br>
     Jurusan: <input type="text" name="jurusan" value="{{ $mhs->jurusan }}"><br>
     <button type="submit">Update</button>
-</form> -->
+</form>
+
 <!DOCTYPE html>
 <html>
 <head>
